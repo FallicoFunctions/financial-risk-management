3dbb54f9a6b2c2c04838724bc4b0a0190c23d8f8
FallicoFunctions <nickf579@gmail.com>
2025-10-27 17:59:25 -0400

    update UserRiskProfileRepository to r2dbc

--- src/main/java/com/nickfallico/financialriskmanagement/repository/UserRiskProfileRepository.java
+++ src/main/java/com/nickfallico/financialriskmanagement/repository/UserRiskProfileRepository.java
@@ -1,9 +1,9 @@
 package com.nickfallico.financialriskmanagement.repository;
 
 import com.nickfallico.financialriskmanagement.model.UserRiskProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRiskProfileRepository extends JpaRepository<UserRiskProfile, String> {
+public interface UserRiskProfileRepository extends R2dbcRepository<UserRiskProfile, String> {
 }
