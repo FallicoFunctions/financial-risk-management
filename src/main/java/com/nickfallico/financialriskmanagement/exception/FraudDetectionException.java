908776b4defc0ce09802e03fb8414a35f5e192a0
FallicoFunctions <nickf579@gmail.com>
2025-10-27 16:37:32 -0400

    FraudDetectionException

---
+++ src/main/java/com/nickfallico/financialriskmanagement/exception/FraudDetectionException.java
@@ -0,0 +1,7 @@
+package com.nickfallico.financialriskmanagement.exception;
+
+public class FraudDetectionException extends RuntimeException {
+    public FraudDetectionException(String message) {
+        super(message);
+    }
+}
