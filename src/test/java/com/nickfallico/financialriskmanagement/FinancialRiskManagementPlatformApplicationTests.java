e45c71de76b79adb07712bcc0e778e0995d33f64
FallicoFunctions <nickf579@gmail.com>
2025-10-28 01:58:05 -0400

    Configure Spring Boot test to disable Redis auto-configuration and caching

--- src/test/java/com/nickfallico/financialriskmanagement/FinancialRiskManagementPlatformApplicationTests.java
+++ src/test/java/com/nickfallico/financialriskmanagement/FinancialRiskManagementPlatformApplicationTests.java
@@ -3,7 +3,16 @@ package com.nickfallico.financialriskmanagement;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+    properties = {
+        // 1. Don't try to spin up Redis-backed cache
+        "spring.cache.type=NONE",
+        // 2. Don't auto-configure Redis at all
+        "spring.autoconfigure.exclude=" +
+        "org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration," +
+        "org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration"
+    }
+)
 class FinancialRiskManagementPlatformApplicationTests {
 
 	@Test
