f3c7e0c65ad3685709fc2c96bf21f16d76133dc3
FallicoFunctions <nickf579@gmail.com>
2025-10-28 01:58:15 -0400

    Configure TransactionProcessingPerformanceTest to disable Redis auto-configuration and caching

--- src/test/java/com/nickfallico/financialriskmanagement/TransactionProcessingPerformanceTest.java
+++ src/test/java/com/nickfallico/financialriskmanagement/TransactionProcessingPerformanceTest.java
@@ -13,7 +13,16 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+@SpringBootTest(
+    properties = {
+        // 1. Don't try to spin up Redis-backed cache
+        "spring.cache.type=NONE",
+        // 2. Don't auto-configure Redis at all
+        "spring.autoconfigure.exclude=" +
+        "org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration," +
+        "org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration"
+    }
+)
 public class TransactionProcessingPerformanceTest {
 
     @Autowired
