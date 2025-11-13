84a566fa1b9dfc7e34bfea8fb7ab4f98c6508760
FallicoFunctions <nickf579@gmail.com>
2025-11-12 20:31:02 -0500

    configure Redis to use JSON serialization with type information

--- src/main/java/com/nickfallico/financialriskmanagement/config/CacheConfig.java
+++ src/main/java/com/nickfallico/financialriskmanagement/config/CacheConfig.java
@@ -10,25 +10,33 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+ 
 @Configuration
 @EnableCaching
 @Profile("!test") // don't load this in tests
 public class CacheConfig {
-
+ 
     @Bean
     public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        // Use JSON serializer that preserves type information
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer();
+ 
         RedisCacheConfiguration userProfileCacheConfig = RedisCacheConfiguration.defaultCacheConfig()
             .entryTtl(Duration.ofMinutes(30))
-            .disableCachingNullValues();
-
+            .disableCachingNullValues()
+            .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer));
+ 
         RedisCacheConfiguration transactionCacheConfig = RedisCacheConfiguration.defaultCacheConfig()
             .entryTtl(Duration.ofMinutes(15))
-            .disableCachingNullValues();
-
+            .disableCachingNullValues()
+            .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer));
+ 
         return RedisCacheManager.builder(redisConnectionFactory)
             .withCacheConfiguration("userProfiles", userProfileCacheConfig)
             .withCacheConfiguration("transactions", transactionCacheConfig)
+            .withCacheConfiguration("merchantFrequencies", userProfileCacheConfig)
             .build();
     }
 }
